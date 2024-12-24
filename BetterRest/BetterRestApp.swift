b1aae5236e7e250226ce8b4ef46678bc19cdf2d8
xHadi3 <hadiessam9@gmail.com>
2024-12-24 12:22:15 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Hadi Al zayer on 23/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
